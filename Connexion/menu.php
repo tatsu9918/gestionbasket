8a8fd154a70af39b5075469f652e6147a54e713e
Theo <wazydrag.theo@gmail.com>
2023-01-26 20:40:12 +0100

    modifcssmenu

--- Connexion/menu.php
+++ Connexion/menu.php
@@ -6,3 +6,31 @@
         <li><a href="/logout.php">Déconnexion</a></li>
     </ul>
 </nav>
+
+<style>
+    nav {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    }
+
+    nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    }
+
+    nav ul li {
+    display: inline-block;
+    }
+
+    nav ul li a {
+    color: #fff;
+    text-decoration: none;
+    padding: 10px 20px;
+    display: block;
+    }
+
+    nav ul li a:hover {
+    background-color: #555;
+    }
