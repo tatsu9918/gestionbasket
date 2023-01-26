59d2425c6ecc8c2cc21157c52ac0937ebf5e9fd6
Tatsu <guillaume.pomies82@gmail.com>
2023-01-26 01:01:00 +0100

    rajout

--- gestionbasket.sql
+++ gestionbasket.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 09 déc. 2022 à 10:45
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
+-- Généré le : jeu. 26 jan. 2023 à 00:00
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -47,15 +47,26 @@ DROP TABLE IF EXISTS `joueurs`;
 CREATE TABLE IF NOT EXISTS `joueurs` (
   `Id_Joueurs` int(11) NOT NULL AUTO_INCREMENT,
   `Nom` varchar(50) DEFAULT NULL,
-  `Prénom` varchar(50) DEFAULT NULL,
+  `Prenom` varchar(50) DEFAULT NULL,
   `Photo` varchar(50) DEFAULT NULL,
   `NumLicence` varchar(50) DEFAULT NULL,
   `DateNaissance` date DEFAULT NULL,
   `Taille` decimal(15,2) DEFAULT NULL,
   `Poids` decimal(15,2) DEFAULT NULL,
+  `Poste_pref` text NOT NULL,
   `Statut` smallint(6) DEFAULT NULL,
+  `Commentaire` text NOT NULL,
   PRIMARY KEY (`Id_Joueurs`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `joueurs`
+--
+
+INSERT INTO `joueurs` (`Id_Joueurs`, `Nom`, `Prenom`, `Photo`, `NumLicence`, `DateNaissance`, `Taille`, `Poids`, `Poste_pref`, `Statut`, `Commentaire`) VALUES
+(18, 'Pomies', 'Guillaume', 'test', '56156', '2023-01-20', '194.00', '92.00', 'Pivot', 1, 'Ã§a va'),
+(25, 'Clotaire', 'Sylvestre', 'test', '648849', '2002-02-24', '181.00', '70.00', 'Ailier', 1, 'test'),
+(26, '2eme', 'Clodomir', 'test', '46584654564', '0495-11-06', '160.00', '70.00', 'Roi', 1, 'test');
 
 -- --------------------------------------------------------
 
