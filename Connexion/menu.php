0a390d8473c9f292ace524f31c07400b8eed838b
Theo <wazydrag.theo@gmail.com>
2023-01-26 20:42:20 +0100

    modifchemin

--- Connexion/menu.php
+++ Connexion/menu.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-        <li><a href="/home.php">Accueil</a></li>
-        <li><a href="/profil.php">Profil</a></li>
-        <li><a href="/settings.php">Paramètres</a></li>
-        <li><a href="/logout.php">Déconnexion</a></li>
+        <li><a href="home.php">Accueil</a></li>
+        <li><a href="profil.php">Profil</a></li>
+        <li><a href="settings.php">Paramètres</a></li>
+        <li><a href="logout.php">Déconnexion</a></li>
     </ul>
 </nav>
 
